7e9372195c27a99ca44f9c785f5fe631c9681ad6
bruno <brmcerqueira@gmail.com>
2019-06-02 10:34:38 -0300

    Start

---
+++ discord-cofbot/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+    application
+    kotlin("jvm") version "1.3.21"
+}
+
+group = "brmcerqueira"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.discord4j:discord4j-core:3.0.6")
+}
+
+
+application {
+    mainClassName = "com.brmcerqueira.discord.cofbot.Main"
+}
---
+++ discord-cofbot/src/main/kotlin/com/brmcerqueira/discord/cofbot/Main.kt
@@ -0,0 +1,27 @@
+package com.brmcerqueira.discord.cofbot
+
+import discord4j.core.event.domain.message.MessageCreateEvent
+import discord4j.core.event.domain.lifecycle.ReadyEvent
+import discord4j.core.DiscordClientBuilder
+import discord4j.core.`object`.entity.Message
+
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val client = DiscordClientBuilder("token").build()
+
+            client.eventDispatcher.on(ReadyEvent::class.java)
+                    .subscribe { ready -> println("Logged in as " + ready.self.username) }
+
+            client.eventDispatcher.on(MessageCreateEvent::class.java)
+                    .map<Message> { it.message }
+                    .filter { msg -> msg.content.map{ "!ping" == it }.orElse(false) }
+                    .flatMap(Message::getChannel)
+                    .flatMap { channel -> channel.createMessage("Pong!") }
+                    .subscribe()
+
+            client.login().block()
+        }
+    }
+}
