5ab5257dc3e51455c058e491f19a187f1c286e58
bruno <brmcerqueira@gmail.com>
2019-06-08 21:28:34 -0300

    Start

--- discord-codbot/src/main/kotlin/com/brmcerqueira/discord/codbot/InitiativeBotMessage.kt
+++ discord-codbot/src/main/kotlin/com/brmcerqueira/discord/codbot/InitiativeBotMessage.kt
@@ -1,12 +1,12 @@
 package com.brmcerqueira.discord.codbot
 
 class InitiativeBotMessage : BotMessage<Int>() {
-    override fun buildMessage(initiative: Int, stringBuffer: StringBuffer) {
+    override fun buildMessage(dto: Int, stringBuffer: StringBuffer) {
         val dice = randomDice()
 
         stringBuffer.appendln("```md")
 
-        stringBuffer.appendln("< $initiative + $dice = ${initiative + dice} >")
+        stringBuffer.appendln("< $dto + $dice = ${dto + dice} >")
 
         stringBuffer.append("```")
     }
